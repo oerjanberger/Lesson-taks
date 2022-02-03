c69ce38c7da1eb412e833dca4e1a159e0baca386
oerjanberger <oerjan4@gmail.com>
2022-02-03 15:07:16 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './Input.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="text-custom-pink">
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p class="text-gray-600 ">Testing</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./public/index.html", ,
+    "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-pink': '#ff00ff',
+      },
+    },
+  },
+  plugins: [],
+}
